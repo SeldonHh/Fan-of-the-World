96852b6759f573ba6d87856221c69c37e00314f6
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-06-19 22:51:20 +0200

    Good beginnings

---
+++ godot/Scenes/MainStuff/fan.gd
@@ -0,0 +1,44 @@
+extends Button
+@onready var fan_texture: TextureRect = %fan_texture
+
+var spinning := false
+var constant_spinning := false
+@export var spinning_time := .25
+const ANIM_IPS := 1.0/20.0
+const TEXTURES: Array[Texture2D] = [
+	preload("uid://diygv6ycq8rmk"),
+	preload("uid://bi7uyupxnl07u"),
+	preload("uid://b3tklwkvfw0mo"),
+	preload("uid://b4btupw8hm2wm"),
+]
+var speed_scale = 1.0
+
+func spin():
+	if !spinning:
+		speed_scale = 1.0
+		var texture_index = 0
+		spinning = true
+		var timer = get_tree().create_timer(spinning_time)
+		timer.timeout.connect(func(): 
+			while constant_spinning :
+				await get_tree().process_frame
+			spinning = false)
+		while spinning:
+			texture_index = texture_index+1
+			if texture_index >= len(TEXTURES):
+				texture_index = 0
+				Global.lower_joules(Global.fanning_power)
+			fan_texture.texture = TEXTURES[texture_index]
+			await get_tree().create_timer(ANIM_IPS/speed_scale).timeout
+			if !constant_spinning:
+				speed_scale -= .1
+			else:
+				speed_scale = 1.0
+	
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Debug"):
+		spin()
+	if Input.is_action_just_pressed("debug2"):
+		constant_spinning = !constant_spinning
+		spin()
---
+++ godot/Scenes/Singleton/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var joules := 99999
+var fanning_power = 1
+
+func lower_joules(amount):
+	joules -= amount
